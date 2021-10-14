ff405fe226048121d3d584d1ce8f6d765176cc2c
xiaozhu260600866 <260600866@qq.com>
2021-10-14 17:40:50 +0800

    xiaozhu

--- temple_platform_wap/pages/staff/index/index.vue
+++ temple_platform_wap/pages/staff/index/index.vue
@@ -29,22 +29,22 @@
 						<view class="p">线上捐赠</view>
 						<view class="num">{{data.payed_count}}<span class="pr10">单</span>{{(putaway1amount + putaway0amount).toFixed(2)}}<span>元</span></view>
 					</view>
-					<view class="sec-group" @click="goto('/pages/boss/offline/lists/index',1)">
+					<!-- <view class="sec-group" @click="goto('/pages/boss/offline/lists/index',1)">
 						<image class="img" src="https://temple.doxinsoft.com/images/wap/boss-I02.png"></image>
 						<view class="p">线下捐赠</view>
 						<view class="num">{{data.payed_count_}}<span class="pr10">单</span>{{data.payed_amount_}}<span>元</span></view>
-					</view>
+					</view> -->
 				</view>
 			</view>
 			<view class="sup-account bg-f pt12 mb30">
-				<view class="name fs-14 fc-7 text-center">数据汇总</view>
+			<!-- 	<view class="name fs-14 fc-7 text-center">数据汇总</view>
 				<view class="flex-center flex-middle">
 					<view class="price fs-30">{{(putaway1amount + putaway0amount).toFixed(2)}}</view>
-				</view>
+				</view> -->
 				<view class="account_nav" style="line-height: 30px;height: 55px;">
 					<view style="height:55px;position: relative;" :class="['nav', putaway == 1 ? 'cur' : '']" @click="putaway=1;online=-1;">线上捐赠(上架)<view style="width:100%;position: absolute;top:25px;">{{putaway1amount.toFixed(2)}}</view></view>
 					<view style="height:55px;position: relative;" :class="['nav', putaway == 0 ? 'cur' : '']" @click="putaway=0;online=-1">线上捐赠(下架)<view style="width:100%;position: absolute;top:25px;">{{putaway0amount.toFixed(2)}}</view></view>
-					<view style="height:55px;position: relative;" :class="['nav', online == 0 ? 'cur' : '']" @click="putaway=-1;online=0;">线下捐赠<view style="width:100%;position: absolute;top:25px;">{{data.payed_amount_}}</view></view>
+					<!-- <view style="height:55px;position: relative;" :class="['nav', online == 0 ? 'cur' : '']" @click="putaway=-1;online=0;">线下捐赠<view style="width:100%;position: absolute;top:25px;">{{data.payed_amount_}}</view></view> -->
 				</view>
 				<view class="ac-group" v-if="putaway != -1">
 					<view class="ac-row" v-for="v in online1Lists" v-if="v.putaway == putaway">
