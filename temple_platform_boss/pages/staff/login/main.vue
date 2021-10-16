bead59812dd497e9a50172e12acc884a16a7afa0
xiaozhu260600866 <260600866@qq.com>
2021-10-16 17:04:40 +0800

    meimei

--- temple_platform_boss/pages/staff/login/main.vue
+++ temple_platform_boss/pages/staff/login/main.vue
@@ -1,22 +1,25 @@
 <template>
 	<view>
 		<page :parentData="data" :formAction="formAction"></page>
+			<div class="logo pt50 pb20">
+				<image class="img" src="/static/logo.jpg"></image>
+			</div>
 			<div class="login-list m15" >
 				<div class="login02">
 					<view class="login-info">
-						<weui-input v-model="ruleform.username" placeholder="帐号" type="text" myclass="plr0" name="username" datatype="require">
+						<weui-input v-model="ruleform.username" placeholder="帐号" type="text" myclass="plr0 ptb15" name="username" datatype="require">
 							<view slot="left" class="slot-icon">
 								<view class="iconfont icon-login-phone"></view>
 							</view>
 						</weui-input>
-						<weui-input v-model="ruleform.password" placeholder="密码" type="password" myclass="plr0" name="password" datatype="require">
+						<weui-input v-model="ruleform.password" placeholder="密码" type="password" myclass="plr0 ptb15" name="password" datatype="require">
 							<view slot="left" class="slot-icon">
 								<view class="iconfont icon-login-passwork"></view>
 							</view>
 						</weui-input>
 					</view>
-					<div class="login-btn mt20">
-						<myform :ruleform="ruleform" :vaildate="vaildate" @callBack="formSubmit" myclass="dx-btn dx-btn-big dx-btn-green bdr4 w-b100 fs-17"
+					<div class="login-btn mt40">
+						<myform :ruleform="ruleform" :vaildate="vaildate" @callBack="formSubmit" myclass="dx-btn dx-btn-big dx-btn-red bdr6 w-b100 fs-20"
 						 title="登录"></myform>
 					</div>
 				</div>
