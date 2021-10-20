67063b07ca53a295e22f4e8f81826df269204bcf
xiaozhu260600866 <260600866@qq.com>
2021-10-20 18:11:41 +0800

    xiaozhu

--- temple_platform_boss/pages/staff/withdraw/index.vue
+++ temple_platform_boss/pages/staff/withdraw/index.vue
@@ -6,7 +6,7 @@
 				<view class="fs-15 fc-6 mb10">可提现金额<text class="Arial plr5">{{putaway1amount.toFixed(2)}}</text>元</view>
 				<view class="withdraw-box">
 					<view class="box-label">提现金额</view>
-					<weui-input v-model="ruleform.amount" myclass="input" placeholder="请输入提现金额" type="text" name="amount" datatype="require|price"></weui-input>
+					<weui-input v-model="ruleform.amount" myclass="input" errorMessage="提现金额" placeholder="请输入提现金额" type="text" name="amount" datatype="require|price"></weui-input>
 				</view>
 			</view>
 			<view class="bg-f type">
@@ -16,7 +16,7 @@
 				<weui-input v-model="ruleform.blank_cardno" label="银行卡号" placeholder="请输入银行卡号" type="text" name="blank_cardno" datatype="require"></weui-input>
 			</view>
 			<view class="m20">
-				<dx-button type="primary" size="lg" round btnBg="#BA9A5D" btnBd="#BA9A5D" block @click="goto('/pages/staff/withdraw/result',1)">提现</dx-button>
+				<dx-button type="primary" size="lg" round btnBg="#BA9A5D" btnBd="#BA9A5D" block @click="submit">提现</dx-button>
 			</view>
 			<view class="fs-14 fc-6 mt10 text-center">满50元可申请提现</view>
 		</view>
@@ -35,6 +35,7 @@
 				data: this.formatData(this),
 				getSiteName: this.getSiteName(),
 				ruleform: {},
+				vaildate:{},
 				bankTypeArr: [
 					{label: '中国工商银行',value: '中国工商银行'},
 					{label: '中国建设银行',value: '中国建设银行'},
@@ -76,6 +77,19 @@
 			this.onShow(this);
 		},
 		methods: {
+			submit(){
+				setTimeout(()=>{
+					this.vaildForm(this, res => {
+						if (res) {
+							if(this.ruleform.amount <50){
+								return this.getError("必须要大于50元才能够提现");
+							}
+							return this.goto('/pages/staff/withdraw/result',1)
+						}
+					})
+				},100);
+				
+			},
 			ajax() {
 				this.getAjax(this,{ searchType: this.searchType,online:this.online }).then(msg => {
 					console.log(this.data);
